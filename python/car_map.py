5a91d4f2254f6e27f4d8dbe9c6b50d58e544577f
hwoo4567 <110660492+hwoo4567@users.noreply.github.com>
2023-08-18 00:27:31 +0900

    ui update

--- python/car_map.py
+++ python/car_map.py
@@ -1,78 +1,8 @@
-import matplotlib
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 import socket
-import asyncio
 import threading
-from tkinter import *
+import ui
 
 # 센서 값을 한 번에 받도록 프로그래밍 하는 것이 좋을 것 같다.
-
-class AppUI(Frame):
-    def __init__(self, parent: Tk):
-        Frame.__init__(self, parent)
-        matplotlib.use("TkAgg", force=True)
-        
-        self.parent = parent
-        self.points = dict()
-        self.loop_func = None  # loop event
-        
-        self.initVar()
-        self.initUI()
-        
-    def initVar(self):
-        self.parent.title("tkinter GUI")
-        self.parent.geometry("500x500+40+40")  # 가로 x 세로 + x좌표 + y좌표
-        self.parent.resizable(True, True)  # 가로 세로 크기 변경 여부
-
-    def initUI(self):
-        # <menu>
-        self.menu_ = Menu(self.parent)
-          # <file>
-        file_menu = Menu(self.menu_, tearoff=False)
-        self.menu_.add_cascade(label="File", menu=file_menu)
-          # <help>
-        help_menu = Menu(self.menu_, tearoff=False)
-        self.menu_.add_cascade(label="Help", menu=help_menu)
-
-        self.parent.config(menu=self.menu_)
-
-        # <main>
-        self.start_btn = Button(self, text="Start", command=self.carStart)
-        self.start_btn.pack(side="bottom", fill="both")
-        
-        fig = Figure(figsize=(10, 10), dpi=128)
-        self.subplt = fig.add_subplot(1, 1, 1)
-        
-        self.canvas = FigureCanvasTkAgg(fig, master=self)
-        self.canvas.draw()
-        self.tkcanvas = self.canvas.get_tk_widget()
-        self.tkcanvas.pack(fill="both")
-        self.tkcanvas.after(50, self.updatePoints)
-
-    def updatePoints(self):
-        for k, v in self.points.items():
-            x, y = k.getPos()
-            v.set_data([x],[y])
-            
-        self.subplt.set_xticks([1, 2, 3, 4, 5])
-        self.subplt.set_yticks([1, 2, 3, 4, 5])
-        self.subplt.grid(True)
-
-        self.canvas.draw()
-        self.tkcanvas.update()
-        self.tkcanvas.after(50, self.updatePoints)
-        
-    def createPoint(self, car):
-        point, = self.subplt.plot(0, 0, marker="o")
-        self.points[car] = point
-        print("Point:", car)
-        
-    def carStart(self):
-        self.start_btn.config(bg="gray", command=lambda: ...)
-        for car in self.points.keys():
-            car.startDrive()
- 
 # 자동차가 항상 위를 보고 있다고 가정   
 class Car:
     port = 1
@@ -80,10 +10,10 @@ class Car:
     def __init__(self, name, mac_address=None):
         self.address = mac_address
         self._name = name
+        self._path = "..."
         self.driving = False
         
         self.x, self.y = 0, 0  # 0 - 5
-        self.dest = 0, 0
         self.light_sensor = 0.0
         self.distance = 0.0
         
@@ -97,39 +27,24 @@ class Car:
     def getPos(self):
         return self.x, self.y
     
+    # /////////////////////////////////////////////////////
+    @property
+    def path(self):
+        return self._path
+    @path.setter
+    def path(self, p):
+        self._path = p
     @property
     def name(self):
         return self._name
     @name.setter
     def name(self, name):
         self._name = name
-    def setDest(self, dest):
-        self.dest = dest
-    def startDrive(self):
-        self.driving = True
     
-    async def drive(self):
-        print(self.driving)
-        if not self.driving:
-            return
-
-        self.go("forward")
-        await asyncio.sleep(5)
-        await asyncio.wait([
-            asyncio.create_task(self.getLightSensor()),
-            asyncio.create_task(self.getDistance()),
-        ])
-        print(self.light_sensor, self.distance)
-
-        self.stop()
-        self.driving = False
-
     def send(self, msg: str):
         self.socket.send(bytes(msg, "ascii"))
-    
     def close(self):
         self.socket.close()
-
     def go(self, direction):
         cmd = f"go {direction}"
         self.send(cmd)
@@ -138,62 +53,43 @@ class Car:
     def turn(self, direction):
         cmd = f"turn {direction}"
         self.send(cmd)
+    def getSensor(self):
+        self.send("sensor")
+        print(self.receive())
+        
+    # /////////////////////////////////////////////////////
 
-    async def receive(self):
-        self.data = ""
+    def receive(self):
+        string_data = ""
         try:
             for _ in range(10):
-                self.data += self.socket.recv(1024).decode("ascii")  # 정보를 받을 때까지 대기
-                if "\n" in self.data:
-                    self.data = self.data.replace("\r", "").replace("\n", "")
-                    print("Received:", self.data)
+                string_data += self.socket.recv(1024).decode("ascii")  # 정보를 받을 때까지 대기
+                if "\n" in string_data:
+                    string_data = string_data.replace("\r", "").replace("\n", "")
+                    print("Received:", string_data)
                     break
         except UnicodeDecodeError:
             print("Received: !!Error!!")
+        finally:
+            return string_data
         
-    async def getLightSensor(self):
-        self.send(f"sensor light")
-        await self.receive()
-        self.light_sensor = float(self.data)
-        
-    async def getLineSensor(self):
-        self.send(f"sensor lineL")
-        await self.receive()
-        print("a")
-        self.send(f"sensor lineR")
-        await self.receive()
-        print("b")
-        
-    async def getDistance(self):
-        self.send("sensor distance")
-        await self.receive()
-        self.distance = float(self.data)
-
-async def main_async(*args: Car):
-    while True:
-        await asyncio.wait([
-            asyncio.create_task(car.drive())
-            for car in args
-        ])
-        await asyncio.sleep(1)
-
 def communication():
-    car1 = Car("test car", "00:22:09:01:FE:87")
-    app.createPoint(car1)
+    car1 = Car("Test Car 1")  # "00:22:09:01:FE:87"
+    car2 = Car("Test Car 2")
+    car3 = Car("Test Car 3")
+    app.register_car(car1)
+    app.register_car(car2)
+    app.register_car(car3)
     
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(main_async(car1))
-    loop.close()
-
-def appExec():
-    root = Tk()
-    global app
-    app = AppUI(root)
+def appDef():
+    global app, root
+    root = ui.Tk()
+    app = ui.AppUI(root)
     app.pack(fill="both", expand=True)
-    root.mainloop()
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=communication)
     t1.daemon = True 
+    appDef()
     t1.start()
-    appExec()
+    root.mainloop()
---
+++ python/ui.py
@@ -0,0 +1,166 @@
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.animation import FuncAnimation
+from tkinter import *
+
+from typing import *
+if TYPE_CHECKING:
+    from car_map import Car
+
+
+class CarOptions(Frame):
+    def __init__(self, parent, car_title, pathway):
+        Frame.__init__(self, parent)
+        
+        self.config(
+            padx=10,
+            pady=10,
+            bg="#dddddd"
+        )
+        
+        # ui
+        left_frame = Frame(self)
+        
+        self.title = Label(left_frame, text=car_title, font=("돋움", 16, "bold"))
+        self.title.pack(side="top", anchor="w", expand=True)
+        
+        self.path_text = Label(left_frame, text=pathway, font=("돋움", 12))
+        self.path_text.pack(side="bottom")
+        
+        left_frame.pack(side="left", fill="y", expand=True)
+        
+        right_frame = Frame(self)
+        
+        self.up_button = Button(right_frame, text="∧", font=("돋움", 16, "bold"))
+        self.up_button.pack(side="top", expand=True, padx=5, pady=5)
+        self.down_button = Button(right_frame, text="∨", font=("돋움", 16, "bold"))
+        self.down_button.pack(side="bottom", expand=True, padx=5, pady=5)
+        
+        right_frame.pack(side="right", fill="both", expand=True)
+        
+        CarOptions.setBgColor(self)
+        
+    def setTitle(self, txt):
+        self.path_text.config(text=txt)
+    def setPath(self, txt):
+        self.path_text.config(text=txt)
+            
+    def setButtonFuncs(self, onUp, onDown):
+        self.up_button.config(command=lambda: onUp(self))
+        self.down_button.config(command=lambda: onDown(self))
+    
+    @classmethod
+    def setBgColor(cls, frame: Frame):
+        for widget in frame.winfo_children():
+            widget.configure(bg="#dddddd")
+            
+            if isinstance(widget, Frame):
+                cls.setBgColor(widget)
+            
+
+class AppUI(Frame):
+    def __init__(self, parent: Tk):
+        Frame.__init__(self, parent)
+        matplotlib.use("TkAgg", force=True)
+        
+        self.parent = parent
+        self.registered_cars: List[Car] = []
+        self.loop_func = None  # loop event
+        
+        self.initVar()
+        self.initUI()
+        
+    def initVar(self):
+        self.parent.title("tkinter GUI")
+        self.parent.geometry("900x600+40+40")  # 가로 x 세로 + x좌표 + y좌표
+        self.parent.resizable(True, True)  # 가로 세로 크기 변경 여부
+
+    def initUI(self):
+        left_frame = Frame(self)
+        
+        fig, self.ax = plt.subplots(figsize=(5, 5))
+        canvas = FigureCanvasTkAgg(fig, master=left_frame)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(fill="both", expand=True)
+
+        self.ani = FuncAnimation(fig, self.updatePoints, interval=200, cache_frame_data=False)
+        self.parent.protocol("WM_DELETE_WINDOW", self.window_close)  # Bind the window close event
+        
+        left_frame.pack(side="left", fill="both", expand=True)
+        
+        self.right_frame = Frame(self, width=300, height=500)
+        
+        Label(self.right_frame, text="Cars", font=("돋움", 20, "bold")).pack()
+        
+        self.car_options: List[CarOptions] = []
+        
+        self.right_frame.pack(side="right", fill="both", expand=True)
+
+    def updatePoints(self, frame):
+        self.ax.clear()  # Clear the previous frame
+        # Draw the grid
+        for x in range(6):
+            self.ax.axvline(x, color='gray', linestyle='--', linewidth=1)
+            self.ax.axhline(x, color='gray', linestyle='--', linewidth=1)
+        
+        # Generate random dot positions
+        dot_positions = [
+            car.getPos()
+            for car in self.registered_cars
+        ]
+        
+        # Draw dots at the randomly generated positions
+        for dot_x, dot_y in dot_positions:
+            self.ax.plot(dot_x, dot_y, 'ro')  # 'ro' means red circle marker
+            self.ax.text(dot_x, dot_y + 0.15, f'({dot_x}, {dot_y})', ha='center', va='bottom', fontsize=16)
+        
+        # Set limits and labels
+        self.ax.set_xlim(0, 5)
+        self.ax.set_ylim(0, 5)
+        self.ax.set_xticks(range(1, 6))
+        self.ax.set_yticks(range(1, 6))
+        self.ax.set_aspect('equal')  # Ensure equal aspect ratio
+        
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        
+        self.ax.set_title("Grid of Cars")
+        
+    def window_close(self):
+        self.ani.event_source.stop()  # Stop the animation
+        plt.close()
+        self.parent.destroy()
+    
+    def register_car(self, car):
+        self.registered_cars.append(car)
+        self.car_options.append(
+            CarOptions(self.right_frame, car.name, car.path)
+        )
+        def up(option: CarOptions):
+            self.car_options.remove(option)
+            self.car_options.insert(0, option)
+            self.update_car_options()
+        def down(option: CarOptions):
+            self.car_options.remove(option)
+            self.car_options.append(option)
+            self.update_car_options()
+            
+        self.car_options[-1].setButtonFuncs(up, down)
+        self.update_car_options()
+        
+    def update_car_options(self):
+        for car, option in zip(self.registered_cars, self.car_options):
+            option.setTitle(car.name)
+            option.setPath(car.path)
+            option.pack_forget()
+            option.pack(side="top", fill="x", padx=10, pady=5)
+
+
+def __main():
+    root = Tk()
+    app = AppUI(root)
+    app.pack(fill="both", expand=True)
+    root.mainloop()          
+if __name__ == "__main__":
+    __main()
